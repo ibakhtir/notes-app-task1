bd2872501507063174c47bcaffcc81ca87890189
ibakhtir <ibakhtir@gmail.com>
2022-09-18 20:26:03 +0300

    developed app

---
+++ src/app/App.js
@@ -0,0 +1,115 @@
+import NotesAPI from "./api/notesApi";
+import NotesView from "./view/notesView";
+import generateId from "./utils/generateId";
+
+export default class App {
+  constructor() {
+    this.notes = [];
+    this.isActiveTable = true;
+    this.view = new NotesView(this.handlers());
+
+    this.refreshNotes();
+  }
+
+  refreshNotes() {
+    const notes = NotesAPI.getNotes();
+
+    const activeNotes = [];
+    const archivedNotes = [];
+    const stats = {};
+
+    // get statistics
+
+    notes.forEach((note) => {
+      if (!stats[note.category]) {
+        stats[note.category] = {
+          active: note.isArchived === false ? 1 : 0,
+          archive: note.isArchived === false ? 0 : 1,
+        };
+      } else {
+        stats[note.category].active += note.isArchived === false ? 1 : 0;
+        stats[note.category].archive += note.isArchived === false ? 0 : 1;
+      }
+    });
+
+    // divide notes into active and archived
+
+    notes.forEach((note) => {
+      if (note.isArchived === true) {
+        archivedNotes.push(note);
+      } else {
+        activeNotes.push(note);
+      }
+    });
+
+    if (this.isActiveTable) {
+      this.setNotes(activeNotes, stats);
+    } else {
+      this.setNotes(archivedNotes, stats);
+    }
+  }
+
+  setNotes(notes, stats) {
+    this.notes = notes;
+
+    // send notes for display
+
+    this.view.updateNotes(notes, stats);
+  }
+
+  handlers() {
+    return {
+      onNoteSelect: (id) => {
+        const selectedNote = this.notes.find((note) => note.id === id);
+        this.view.setActiveNote(selectedNote);
+
+        this.refreshNotes();
+      },
+
+      onNoteAdd: (title, body, category, dates) => {
+        const newNote = {
+          id: generateId(),
+          title,
+          body,
+          category,
+          createdAt: Date.now(),
+          dates,
+          isArchived: false,
+        };
+
+        NotesAPI.saveNote(newNote);
+        this.refreshNotes();
+      },
+
+      onNoteEdit: (id, title, body, category, dates) => {
+        NotesAPI.saveNote({
+          id,
+          title,
+          body,
+          category,
+          dates,
+        });
+
+        this.refreshNotes();
+      },
+
+      onNoteDelete: (id) => {
+        NotesAPI.deleteNote(id);
+
+        this.refreshNotes();
+      },
+
+      onNoteArchive: (note) => {
+        NotesAPI.archiveNote(note);
+
+        this.refreshNotes();
+      },
+
+      onChangeTableView: (status) => {
+        this.isActiveTable = status;
+
+        this.refreshNotes();
+      },
+    };
+  }
+}
---
+++ src/app/view/notesView.js
@@ -0,0 +1,233 @@
+import Modal from "../common/modal";
+import { getDate, getDateFromText } from "../utils/getDate";
+
+import pencilImg from "../assets/pencil.svg";
+import archiveImg from "../assets/archive.svg";
+import unarchiveImg from "../assets/unarchive.svg";
+import trashImg from "../assets/trash.svg";
+
+export default class NotesView {
+  constructor({
+    onNoteSelect,
+    onNoteAdd,
+    onNoteEdit,
+    onNoteDelete,
+    onNoteArchive,
+    onChangeTableView,
+  }) {
+    this.onNoteSelect = onNoteSelect;
+    this.onNoteAdd = onNoteAdd;
+    this.onNoteEdit = onNoteEdit;
+    this.onNoteDelete = onNoteDelete;
+    this.onNoteArchive = onNoteArchive;
+    this.onChangeTableView = onChangeTableView;
+
+    this.activeNote = null;
+    this.isActiveTable = true;
+    this.errors = false;
+    this.modal = new Modal();
+
+    document.addEventListener("click", this.action.bind(this));
+  }
+
+  setActiveNote(note) {
+    this.activeNote = note;
+  }
+
+  action(event) {
+    const { target } = event;
+
+    // get active note
+
+    const activeNoteHTML = target.closest(".list-table__item");
+
+    if (activeNoteHTML) {
+      const activeNoteId = activeNoteHTML.dataset.noteId;
+      this.onNoteSelect(activeNoteId);
+    }
+
+    // open modal for new note
+
+    if (target.closest(".notes__add")) {
+      this.modal.show();
+    }
+
+    // open modal for edit app
+
+    if (target.closest(".list-table__edit")) {
+      this.modal.show(this.activeNote);
+    }
+
+    // delete note
+
+    if (target.closest(".list-table__delete")) {
+      this.onNoteDelete(this.activeNote.id);
+      this.activeNote = null;
+    }
+
+    // archive note
+
+    if (target.closest(".list-table__archive")) {
+      if (this.isActiveTable) {
+        this.onNoteArchive({
+          ...this.activeNote,
+          isArchived: true,
+        });
+      } else {
+        this.onNoteArchive({
+          ...this.activeNote,
+          isArchived: false,
+        });
+      }
+
+      this.activeNote = null;
+    }
+
+    // show archive instead of active notes
+
+    if (target.closest(".notes__archive")) {
+      this.isActiveTable = !this.isActiveTable;
+
+      this.onChangeTableView(this.isActiveTable);
+
+      const archiveBtn = document.querySelector(".notes__archive");
+      archiveBtn.textContent = `${this.isActiveTable ? "Archive" : "Active"}`;
+    }
+
+    // add or edit note
+
+    if (target.closest(".form__save")) {
+      const inputTitle = document.querySelector(".form__title");
+      const inputBody = document.querySelector(".form__body");
+      const selectedCategory = document.querySelector(".form__options");
+
+      const titleValue = inputTitle.value.trim();
+      const bodyValue = inputBody.value.trim();
+      const categoryValue = selectedCategory.value.trim();
+
+      const datesArr = getDateFromText(bodyValue) || [];
+      const dates = datesArr.join(", ");
+
+      // basic validation
+
+      if (titleValue === "" || bodyValue === "") {
+        this.errors = true;
+        const error = this.createErrorHTML("All fields are required");
+        inputTitle.insertAdjacentElement("beforebegin", error);
+      } else {
+        this.errors = false;
+      }
+
+      // saving changes
+
+      if (!this.errors) {
+        if (this.activeNote) {
+          this.onNoteEdit(
+            this.activeNote.id,
+            titleValue,
+            bodyValue,
+            categoryValue,
+            dates
+          );
+        } else {
+          this.onNoteAdd(titleValue, bodyValue, categoryValue, dates);
+        }
+
+        this.modal.hide();
+        this.activeNote = null;
+      }
+    }
+
+    // hide modal
+
+    if (target.closest(".form__cancel")) {
+      this.modal.hide();
+    }
+  }
+
+  updateNotes(notes, stats) {
+    // send data for display notes
+
+    const notesList = document.querySelector(".list-table__body");
+    notesList.innerHTML = "";
+
+    notes.forEach((note) => {
+      const noteItemHTML = this.createNotesHTML(note);
+      notesList.append(noteItemHTML);
+    });
+
+    // send data for display statistics
+
+    const totalList = document.querySelector(".total-table__body");
+    totalList.innerHTML = "";
+
+    Object.keys(stats).forEach((item) => {
+      const totalItemHTML = this.createCategoryHTML(
+        item,
+        stats[item].active,
+        stats[item].archive
+      );
+      totalList.append(totalItemHTML);
+    });
+  }
+
+  // methods for display
+
+  createNotesHTML(note) {
+    const { id, title, body, category, createdAt, dates } = note;
+
+    const tableBodyRow = document.createElement("tr");
+    tableBodyRow.className = "list-table__item";
+    tableBodyRow.dataset.noteId = id;
+    tableBodyRow.innerHTML = `
+      <td>${title}</td>
+      <td class="list-table__content">${body}</td>
+      <td>${category}</td>
+      <td>${getDate(createdAt, "full")}</td>
+      <td>${dates}</td>
+      <td class="list-table__actions">
+        <div>
+          ${
+            this.isActiveTable
+              ? `<button type="button" class="list-table__edit"><img src=${pencilImg} alt="Pencil" /></button>`
+              : ""
+          }
+          <button type="button" class="list-table__archive">${
+            this.isActiveTable
+              ? `<img src=${archiveImg} alt="Archive" />`
+              : `<img src=${unarchiveImg} alt="Unarchive" />`
+          }</button>
+          <button type="button" class="list-table__delete"><img src=${trashImg} alt="Trash" /></button>
+        </div>
+      </td>
+    `;
+
+    return tableBodyRow;
+  }
+
+  createCategoryHTML(category, active, archive) {
+    const tableBodyRow = document.createElement("tr");
+    tableBodyRow.className = "total-table__item";
+    tableBodyRow.innerHTML = `
+      <td>${category}</td>
+      <td>${active}</td>
+      <td>${archive}</td>
+    `;
+
+    return tableBodyRow;
+  }
+
+  createErrorHTML(message) {
+    const isErrorBlock = document.querySelector(".form__error");
+
+    if (isErrorBlock) {
+      isErrorBlock.remove();
+    }
+
+    const errorBlock = document.createElement("span");
+    errorBlock.className = "form__error";
+    errorBlock.textContent = `${message}`;
+
+    return errorBlock;
+  }
+}
